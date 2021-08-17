41c8f49269207b63191dbf1301d427cce534bcc0
SS1ster <zyferum@mail.ru>
2021-08-17 11:17:21 +0700

    add

---
+++ DZ 5.2/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Net.Http;
+
+namespace DZ 5.2{
+    class Class2
+    {
+        static string siteString = "";
+        public static void Message(char delWordOnChar)
+        {
+            Random random = new Random();
+            int numCharInWord = random.Next(4, 10);
+            getString();
+            List<string> words = new List<string>();
+            words = WordFixSize(siteString, numCharInWord);
+            Console.WriteLine("Слова не более чем " + numCharInWord);
+            foreach (string str in words)
+            {
+                Console.WriteLine(str);
+            }
+            char charForDel = (char)random.Next('а', 'я');
+            for (int i = 0; i < words.Count; i++)
+            {
+                if (words[i][words[i].Length - 1] == charForDel)
+                {
+                    words.Remove(words[i]);
+                }
+            }
+
+            Console.WriteLine("Слова не оканчивающиеся на " + charForDel + "===============================");
+            foreach (string str in words)
+            {
+                Console.WriteLine(str);
+            }
+            Console.WriteLine("Самое длинное слово " + maxWord(siteString) + "===============================");
+            Console.ReadLine();
+        }
+        static string maxWord(string stringIn)
+        {
+            string tempStr = "";
+            string[] wordsAll = stringIn.Split(' ');
+            tempStr = wordsAll[0];
+            foreach (string str in wordsAll)
+            {
+                if (str.Length > tempStr.Length)
+                {
+                    tempStr = str;
+                }
+            }
+            return tempStr;
+        }
+        static void getString()
+        {
+            string url = @"https://fpgame.ru/news/news_page.html";
+            HttpClient client = new HttpClient();
+            var req = client.GetStringAsync(url).Result;
+            int posStart = req.IndexOf("<body>") + "<body>".Length;
+            int posEnd = req.IndexOf("</body>");
+            int length = posEnd - posStart;
+            siteString = req.Substring(posStart, length);
+        }
+        static List<string> WordFixSize(string message, int numCharInWord)
+        {
+            string str = "";
+            string[] wordsAll = message.Split(' ');
+            List<string> words = new List<string>();
+            char[] chDel = { ' ', ',', '.', ';', ':', '<', '>', '/' };
+            string[] strDel =
+            {
+                "\n", "<p", "<div>\n", "<tabl>\n", "<tr>\n","<th","<nav>\n","<ul",
+                "<li","<a","</a>\n","</li>\n","</ul>\n","</nav>\n","</th>\n"
+            };
+            foreach (string st in wordsAll)
+            {
+                if (st.Length > 0)
+                {
+                    words.Add(st);
+                }
+            }
+            for (int i = 0; i < words.Count; i++)
+            {
+                foreach (string stringForDel in strDel)
+                {
+                    if (words[i] == stringForDel)
+                    {
+                        words.Remove(stringForDel);
+                    }
+                }
+                if (words[i].Length > numCharInWord)
+                {
+                    string temp = words[i];
+                    words.Remove(temp);
+                }
+            }
+
+            return words;
+        }
+    }
+}
--- DZ 5/Program.cs
+++ DZ 5/Program.cs
@@ -1,12 +1,107 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
-namespace DZ_5
+namespace DZ 5{
+
+    class Class1
 {
-    class Program
+    string login = "";
+    string pasword = "";
+    public void CheckInputDataNotReg()
     {
-        static void Main(string[] args)
+        bool chekEnter = false;
+        do
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите логин:");
+            string tmpLogin = Console.ReadLine();
+
+            if (tmpLogin.Length < 2) Console.WriteLine("Логин слишком короткий!");
+            else
+            {
+                if (tmpLogin.Length > 10) Console.WriteLine("Логин слишком длинный!");
+                else
+                {
+                    foreach (char ch in tmpLogin)
+                    {
+                        if (
+                            (ch < '0') ||
+                            (ch > '9' && ch < 'A') ||
+                            (ch > 'Z' && ch < 'a') ||
+                            (ch > 'z')
+                          )
+                        {
+                            Console.WriteLine("Не верный символ в логине, разрешены только латинские символы и цифры.");
+                            break;
+                        }
+                        else
+                        {
+                            if (tmpLogin[0] < 'A')
+                            {
+                                Console.WriteLine("Первым символом должна быть буква.");
+                                break;
+                            }
+                            else
+                            {
+                                login = tmpLogin;
+                                chekEnter = true;
+
+                            }
+                        }
+                    }
+                }
+            }
         }
+        while (!chekEnter);
+        Console.WriteLine("Введите пароль:");
+        pasword = Console.ReadLine();
+        Console.WriteLine("Логин: " + login + ", Пароль: " + pasword);
+        Console.ReadLine();
     }
+
+    public void CheckInputDataReg()
+    {
+        login = "";
+        Console.WriteLine("Введите логин:");
+        bool chekEnter = false;
+        do
+        {
+            string tmpLogin = Console.ReadLine();
+            if (tmpLogin.Length < 2) Console.WriteLine("Логин слишком короткий!");
+            else
+            {
+                if (tmpLogin.Length > 10) Console.WriteLine("Логин слишком длинный!");
+                else
+                {
+                    Regex regexStart = new Regex(@"\b[0-9]");
+                    if (regexStart.IsMatch(tmpLogin))
+                    {
+                        Console.WriteLine("Первым символом должна быть буква.");
+                    }
+                    else
+                    {
+                        Regex regex = new Regex(@"[0-9A-Za-z]");
+                        if (!regex.IsMatch(tmpLogin))
+                        {
+                            Console.WriteLine("Символы не соответствуют.");
+                        }
+                        else
+                        {
+                            login = tmpLogin;
+                            chekEnter = true;
+                        }
+                    }
+                }
+            }
+        }
+        while (!chekEnter);
+        Console.WriteLine("Введите пароль:");
+        pasword = Console.ReadLine();
+        Console.WriteLine("Логин: " + login + ", Пароль: " + pasword);
+        Console.ReadLine();
+    } 
+}
 }
